90e7da8b3790cee9a252dd963d158a0e6642c0f1
Emiliano Presa custodio <epresa-c@c2r8s9.42lausanne.ch>
2022-11-21 14:07:20 +0100

    rename binary ex02

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,10 +6,14 @@ int main( void )
 	Fixed a;
 	Fixed b( a );
 	Fixed c;
+	// Fixed e;
+
 	c = b;
+	// e.setRawBits(55);
 
 	std::cout << a.getRawBits() << std::endl;
 	std::cout << b.getRawBits() << std::endl;
 	std::cout << c.getRawBits() << std::endl;
+	// std::cout << e.getRawBits() << std::endl;
 	return 0;
 }
